cb4f41bd564319991d331d7f174f33d1e6312e60
Ahmad Zaheer <ahmadzaheerdev@gmail.com>
2023-11-26 01:54:44 -0800

    NextAuth options moved to separate file

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,48 +1,5 @@
 import NextAuth from "next-auth/next";
-import { type NextAuthOptions } from "next-auth";
-import { PrismaAdapter } from "@auth/prisma-adapter";
-import CredentialsProvider from "next-auth/providers/credentials";
-import prisma from "@/lib/prisma";
-import type { User } from "@prisma/client";
-
-export const authOptions: NextAuthOptions = {
-  adapter: PrismaAdapter(prisma),
-  session: {
-    strategy: "jwt",
-  },
-  providers: [
-    CredentialsProvider({
-      name: "credentials",
-      credentials: {
-        email: { label: "Email", type: "text", placeholder: "jsmith" },
-        password: { label: "Password", type: "password" },
-      },
-      async authorize(credentials) {
-        const email = credentials?.email;
-        const password = credentials?.password;
-
-        if (!email || !password) {
-          throw new Error("email or password not provided");
-        }
-
-        const user: User | null = await prisma.user.findUnique({
-          where: { email: email },
-        });
-
-        if (!user) {
-          throw new Error(`No user found with email: ${email}`);
-        }
-
-        return user;
-      },
-    }),
-  ],
-  pages: {
-    signIn: "/az-login",
-  },
-  secret: process.env.NEXTAUTH_SECRET,
-  debug: process.env.NODE_ENV === "development",
-};
+import authOptions from "@/lib/authOptions";
 
 const handler = NextAuth(authOptions);
 
